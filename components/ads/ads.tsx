891ca92ac6a4f1910bbb002de6ed50f5c5f0bbb2
Lucifer472 <hardiksadhu472@gmail.com>
2024-12-26 16:27:10 +0530

    initial Commit

---
+++ components/ads/ads.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { AdsWrapper } from "@/components/wrappers/ad-wrapper";
+
+export const Ad1 = () => {
+  return <AdsWrapper id="2586291034" />;
+};
+
+export const Ad2 = () => {
+  return <AdsWrapper id="8960127691" />;
+};
+
+export const Ad3 = () => {
+  return <AdsWrapper id="6333964355" />;
+};
+
+export const Ad4 = () => {
+  return <AdsWrapper id="2394719341" />;
+};
+
+export const Ad5 = () => {
+  return <AdsWrapper id="7332217229" />;
+};
---
+++ constant/index.ts
@@ -0,0 +1,68 @@
+export const category = [
+  {
+    label: "Government yojnaa",
+    link: "/government-yojnaa",
+  },
+  {
+    label: "Application",
+    link: "/application",
+  },
+  {
+    label: "News",
+    link: "/news",
+  },
+  {
+    label: "Government Schemes",
+    link: "/government-schema",
+  },
+  {
+    label: "Scholarship",
+    link: "/scholarship",
+  },
+  {
+    label: "Jobs",
+    link: "/jobs",
+  },
+  {
+    label: "Exams",
+    link: "/exams",
+  },
+];
+
+export const footerLink = [
+  {
+    link: "/about-us",
+    label: "About Us",
+  },
+  {
+    link: "/contact-us",
+    label: "Contact Us",
+  },
+  {
+    link: "/author",
+    label: "Author",
+  },
+  {
+    link: "/privacy-policy",
+    label: "Privacy",
+  },
+  {
+    link: "/terms",
+    label: "Terms & Condition",
+  },
+  {
+    link: "/disclaimer",
+    label: "Disclaimer",
+  },
+];
+
+export const byFile = "https://images.drivingexamexpert.com/upload/blog.php";
+export const byUrl = "https://images.drivingexamexpert.com/upload/url.php";
+export const byProfile =
+  "https://images.drivingexamexpert.com/upload/profile.php";
+
+export const url = "https://blog.hellogandhinagar.com";
+export const title = "Blog G";
+export const description = `Welcome to blog.hellogandhinagar.com, your go-to resource for comprehensive Driving Exam Blogs in Australia. Whether you're preparing for your learner's permit or aiming to ace your practical driving test, we provide valuable insights, expert tips, and firsthand experiences tailored to the Australian driving landscape. Navigate the road to success with our in-depth guides on driving test routes, regional road rules, and city-specific exam strategies. Stay updated on the latest changes in Australian driving regulations and boost your confidence behind the wheel. Join our community of learners and experienced drivers as we share the knowledge that drives success on the Australian roads. Start your journey to driving excellence today with blog.hellogandhinagar.com!`;
+export const keywords = "";
+export const CardImage = "/images";
